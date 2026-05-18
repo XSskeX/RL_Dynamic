bcfe903117a9804fd1cc501d6cc50874a1f91a75
unknown <1097484850@qq.com>
2026-05-18 17:25:39 +0800

    add compute_cosine_drift

--- diffing-toolkit_for_RL/activation_analysis.py
+++ diffing-toolkit_for_RL/activation_analysis.py
@@ -6,11 +6,11 @@ import pandas as pd
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from loguru import logger
-
+from torch.nn.functional import cosine_similarity
 from diffing.pipeline.diffing_pipeline import DiffingPipeline
 from diffing.pipeline.evaluation_pipeline import EvaluationPipeline
 from diffing.utils.configs import CONFIGS_DIR
-from ..configs import get_model_configurations, get_dataset_configurations, get_nway_model_configurations
+from diffing.utils.configs import get_model_configurations, get_dataset_configurations, get_nway_model_configurations
 
 def activation_analysis():
     model_path = f"{cfg.infrastructure.storage.checkpoint_dir}/{cfg.model.name}/model_final.pt"
@@ -48,8 +48,29 @@ def compute_feature_norm(crosscoder):
 
         latent_df[f"dec_{i}_norm"] = ratios.detach().numpy()
 
-    latent_df.to_csv(Path(f"/share/nlp/baijun/shuhan/crosscoder_output/crosscoder_output_for_4/{model_name}_latent_norm.csv"), encoding='utf-8-sig')
+    latent_df.to_csv(Path(f"/share/nlp/baijun/shuhan/crosscoder_output/crosscoder_output_for_4/llama32_3B_Instruct_latent_norm.csv"), encoding='utf-8-sig')
     return latent_df
+
+def _compute_feature_drift_cosine(weight, original_weight):
+    weight = weight.float()
+    original_weight = original_weight.float()
+    eps = 1e-8
+    similarity = cosine_similarity(weight, original_weight, dim=1, eps=eps)
+    return 1.0 - similarity
+
+def compute_feature_drift_cosine(crosscoder):
+    num_features = crosscoder.decoder.weight.shape[1]
+    latent_df = pd.DataFrame(index=range(num_features))
+    original_weight = crosscoder.decoder.weight[0]
+    for i in range(1, crosscoder.decoder.weight.shape[0]):
+        weight = crosscoder.decoder.weight[i]
+
+        ratios = _compute_feature_drift_cosine(weight, original_weight).cpu()
+        latent_df[f"dec_{i}_drift_from_origin"] = ratios.detach().numpy()
+    latent_df.to_csv(Path(f"/share/nlp/baijun/shuhan/crosscoder_output/crosscoder_output_for_4/llama32_3B_Instruct_latent_drift.csv"), encoding='utf-8-sig')
+    return latent_df
+
+
 
 @hydra.main(version_base=None, config_path=str(CONFIGS_DIR), config_name="config")
 def main(cfg: DictConfig) -> None:
